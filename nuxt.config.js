5812a2881ff49c1866fdd884642017d6da68d8c3
leprokuda <leprokuda@gmail.com>
2022-11-21 21:07:38 +0600

    Подготовка к публикации

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,8 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
   target: 'static',
-  generate: {
-    dir: 'crm'
+  router: {
+    base: '/crm-vue/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
